0e0604fcb286742c5c6e05a59c044a8f75824fcb
najammehar <najamulhassan1033@gmail.com>
2025-09-11 15:16:58 +0500

    fix: deleted types.js

--- admin/lib/types.js
+++
@@ -1,8 +0,0 @@
-export type Room {
-  id: string;            // uuid
-  room: string;          // text
-  day: string;           // text
-  time: string;          // text
-  occupied: boolean;     // boolean
-  createdAt: string;     // timestamptz (ISO string)
-}
